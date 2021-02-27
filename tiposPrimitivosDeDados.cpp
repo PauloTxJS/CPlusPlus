1930c0071d759ccc576abf9d1d0bab353c3e4f17
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-02-26 21:45:27 -0300

    Tipos primitivos de dados em c++

---
+++ tiposPrimitivosDeDados.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string name = "Paulo";
+    int age = 35;
+    float weight = 95.0;
+    double height = 1.80;
+    bool smoker = false;
+    char genre = 'M';
+
+    cout << name << endl;
+    cout << age << endl;
+    cout << weight << endl;
+    cout << height << endl;
+    cout << smoker << endl;
+    cout << genre << endl;
+
+
+    return 0;
+}
