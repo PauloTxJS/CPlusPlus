f4b632e1b0c5237a94204bcc09ba280457842e5d
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-02-26 21:53:28 -0300

    Operadores aritméticos

---
+++ operadoresAritmeticos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n1 = 10;
+    int n2 = 2;
+    int soma = n1 + n2;
+    int diferenca = n1 - n2;
+    int produto = n1 * n2;
+    int quociente = n1 / n2;
+    int modulo = n1 % n2;
+
+    cout << soma << endl;
+    cout << diferenca << endl;
+    cout << produto << endl;
+    cout << quociente << endl;
+    cout << modulo << endl;
+    
+    return 0;
+}
