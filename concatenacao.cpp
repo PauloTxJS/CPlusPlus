f69ce8f4952be7960962086f36871dec827b9331
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-02-27 09:07:54 -0300

    Concatenação e loop 'while'

---
+++ concatenacao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string name = "Paulo";
+    int age = 35;
+
+    cout << "Hey! My name is " << name << " and I am " << age << " years old." << endl;
+
+    return 0;
+}
---
+++ loopWhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n = 0;
+
+    while(n <= 10) {
+        cout << n << endl;
+        n++;
+    }
+
+    return 0;
+}
